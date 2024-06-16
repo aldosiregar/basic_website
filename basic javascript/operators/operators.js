e0ae274090aef248c28563e32cba9780e48095d8
aldosiregar <muhammadaldorafausta@gmail.com>
2024-06-16 19:30:51 +0700

    tambah tutorial operator

---
+++ basic javascript/operators/operators.js
@@ -0,0 +1,94 @@
+/**
+ * operator javascript digunakan untuk melakukan berbagai proses komputasi matematika dan logika
+ * 
+ * beberapa tipe operator javascript adalah antara lain : 
+ *  1. arithmethic operators
+ *  2. assignment operators
+ *  3. comparison operators
+ *  4. string operators
+ *  5. logical operators
+ *  6. bytewise operators
+ *  7. ternary operators
+ *  8. type operators
+ */
+
+//javascript arithmethic operators
+/**
+ * operator arithmetic digunakan untuk melakukan proses perhitungan matematika 
+ * 
+ * Operator	    Description
+ * (+)	        Addition
+ * (-)	        Subtraction
+ * (*)	        Multiplication
+ * (**)	        Exponentiation (ES2016)
+ * (/)	        Division
+ * (%)	        Modulus (Division Remainder)
+ * (++)	        Increment
+ * (--) 	    Decrement  
+ */ 
+
+//javascript assignment operators
+/**
+ * assignment operators digunakan untuk melakukan proses penugasan pada sebuah variabel
+ * 
+ * Operator     Example	        Same As
+ * =	        x = y	        x = y
+ * +=	        x += y	        x = x + y
+ * -=	        x -= y	        x = x - y
+ * *=	        x *= y	        x = x * y
+ * /=	        x /= y	        x = x / y
+ * %=	        x %= y	        x = x % y
+ * **=	        x **= y	        x = x ** y
+ */
+
+//javascript comparison operators
+/**
+ * comparison operators digunakan untuk membandingkan 2 variabel dalam javascript
+ * 
+ * operators    description
+ * (==)	        equal to
+ * (===)	    equal value and equal type
+ * (!=)	        not equal
+ * (!==)	    not equal value or not equal type
+ * (>)	        greater than
+ * (<)	        less than
+ * (>=)	        greater than or equal to
+ * (<=)	        less than or equal to
+ * (?)	        ternary operator
+ */
+
+//javascript logical operators
+/**
+ * logical operators digunakan untuk melakukan proses logika
+ * 
+ * Operator	    Description
+ * (&&)	        logical and
+ * (||)	        logical or
+ * (!)	        logical not
+ */
+
+//javascript type operators
+/**
+ * type operators digunakan untuk proses yang berhubungan dengan tipe data sebuah variabel
+ * 
+ * Operator	    Description
+ * typeof	    Returns the type of a variable
+ * instanceof	Returns true if an object is an instance of an object type
+ */
+
+//javascript bitwise operators
+/**
+ * bit operators bekerja pada angka 32 bit
+ * 
+ * semua angka yang ingin dioperasikan akan diubah ke 32 bit
+ * kemudian hasilnya akan diubah kembali ke angka javascript
+ * 
+ * Operator	    Description	            Example	    Same as	        Result	Decimal
+ * &	        AND	                    5 & 1	    0101 & 0001	    0001	 1
+ * |	        OR	                    5 | 1	    0101 | 0001	    0101	 5
+ * ~	        NOT	                    ~ 5	        ~0101	        1010	 10     
+ * ^	        XOR	                    5 ^ 1	    0101 ^ 0001	    0100	 4
+ * <<	        left shift	            5 << 1	    0101 << 1	    1010	 10
+ * >>	        right shift             5 >> 1	    0101 >> 1	    0010	  2
+ * >>>	        unsigned right shift	5 >>> 1	    0101 >>> 1	    0010	  2
+ */
